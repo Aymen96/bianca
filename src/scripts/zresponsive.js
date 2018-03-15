9fbffc794088ee52e736cc45fea9083a7945486d
Aymen Hachicha <aymenhachicha96@gmail.com>
2018-03-15 12:27:58 +0100

    first base responsive development

--- src/scripts/zresponsive.js
+++ src/scripts/zresponsive.js
@@ -1,13 +1,46 @@
 $(document).ready(function () {
 
     function adjustContent() {
-        if (screen.width <= 480) {
-            console.log("YOO PHONE");
+        if (screen.width <= 600 || screen.height <= 480) {
+            console.log("YOO phone");
             $(".persons-hover-div").remove();
             $(".names").remove();
             $(".theme-slick").remove();
-            $(".small-devices-header").removeClass("hidden");
-        } else {
+            $(".header").addClass("small-devices-header").removeClass("header").empty().append('    <div class="header-top clearfix">\n' +
+                '<a class="float-right toggle-menu" href="#">\n' +
+                '<i></i>\n' +
+                '<i></i>\n' +
+                '<i></i>\n' +
+                '</a>\n' +
+                '</div>\n' +
+                '<nav class="hide nav-me">\n' +
+                '<ul id="menu">\n' +
+                '<li class="themen active-header-item">Themen</li>\n' +
+                '<li class="personen">Personen</li>\n' +
+                '<li>Info</li>\n' +
+                '<li>Mitmachen</li>\n' +
+                '</ul>\n' +
+                '</nav>');
+        } else if(screen.width <= 1024) {
+            $(".persons-hover-div").remove();
+            console.log("YOO tablet");
+            $(".header").addClass("small-devices-header").removeClass("header").empty().append('    <div class="header-top clearfix">\n' +
+                '<a class="float-right toggle-menu" href="#">\n' +
+                '<i></i>\n' +
+                '<i></i>\n' +
+                '<i></i>\n' +
+                '</a>\n' +
+                '</div>\n' +
+                '<nav class="hide nav-me">\n' +
+                '<ul id="menu">\n' +
+                '<li class="themen active-header-item">Themen</li>\n' +
+                '<li class="personen">Personen</li>\n' +
+                '<li>Info</li>\n' +
+                '<li>Mitmachen</li>\n' +
+                '</ul>\n' +
+                '</nav>');
+        }
+        else {
             console.log("YOO laptop");
         }
     }
@@ -31,7 +64,7 @@ $(document).ready(function () {
         });
 
     // hide menu when clicking on the menu items or the logo
-        $(' #menu li a').click(function(){
+        $('#menu li a').click(function(){
             $header_top.removeClass('open-menu');
         });
 });
